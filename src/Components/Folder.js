608885bdea9640f0fb7c7d89dcf5bb4b355ea77e
Snakeshader1 <theo.stoffelbach@epitech.digital>
2022-05-28 23:53:25 +0200

    V1.0.0

--- src/Components/Folder.js
+++ src/Components/Folder.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import all_Projet from "../asset/Tous_les_projets.zip";
+import Projet_42 from "../asset/Projet_42.zip";
 
-const Folder = ({ text }) => {
-  const lang = "fr";
-  let url = `/window/${text}-${lang}`;
+const Folder = ({ text, folder }) => {
+  // const lang = "fr";
+  // let url = `/window/${text}-${lang}`;
+
+  if (folder === "Sources") folder = "https://catalystvibes.dribbble.com/";
+  else if (folder === "all_Projet") folder = all_Projet;
+  else if (folder === "Projet_42") folder = Projet_42;
+  else folder = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+  function test() {
+    console.log(folder);
+  }
 
   return (
     <div className="file">
-      <a href={"/"} download>
+      <a href={folder} download onClick={test}>
         <div className="folder_canva_img">
           <div className="languette"></div>
           <div className="folder_img"></div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import File from "./File";
 import Folder from "./Folder";
 
 const Home = () => {
-  const [page, setPage] = useState("");
-
   return (
     <div>
       <div className="Computer">
         <div className="grid">
           <File text={"CV.pdf"} />
-          <Folder text={"Porte_folio"} />
-          <Folder text={"Sources"} />
-          <Folder text={"Features"} />
-          <Folder text={"New text"} />
+          <Folder text={"Porte_folio"} folder={"all_Projet"} />
+          <Folder text={"Sources"} folder={"Sources"} />
+          <Folder text={"future fonctionnalités"} folder={"Projet_42"} />
+          <Folder text={"New text"} folder={"Rick"} />
         </div>
       </div>
       <div className="pied de l'ordi">
